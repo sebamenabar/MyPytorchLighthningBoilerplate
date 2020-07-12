8e0c56c6726fc118718845f5275b59ca947534c2
Sebastian Amenabar <samenabar@uc.cl>
2020-07-12 18:12:00 -0400

    Improvements from ConceptMac

--- src/main.py
+++ src/main.py
@@ -1,114 +1,69 @@
+import os
+import sys
+import os.path as osp
+from pprint import PrettyPrinter as PP
+
 from easydict import EasyDict as edict
 from dotenv import load_dotenv
 
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
-from base_pl_model import BasePLModel
+from plb_base.base_pl_model import BasePLModel, Logger, ErrLogger
 from config import __C, parse_args_and_set_config
-
-### EXAMPLE SPECIFIC
-import os
-from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
-from torchvision import transforms
 
 load_dotenv()
 
-__C.model = edict(
-    in_dim=(784, edict(help="input dimension", type=int)),
-    hidden_dim=(1000, edict(help="hidden dim", type=int)),
-    out_dim=(10, edict(help="output dimension", type=int)),
-)
-
-
-class Model(BasePLModel):
-    def __init__(self, hparams=None):
-        super().__init__(hparams=hparams)
-
-        model_cfg = self.cfg.model
-        self.layers = nn.Sequential(
-            nn.Linear(784, model_cfg.in_dim, model_cfg.hidden_dim),
-            nn.ReLU(),
-            nn.Linear(784, model_cfg.hidden_dim, model_cfg.out_dim),
-        )
-        self.loss = nn.CrossEntropyLoss()
-
-    def train_dataloader(self):
-        dataset = MNIST(
-            os.getcwd(), train=True, download=True, transform=transforms.ToTensor()
-        )
-        loader = DataLoader(
-            dataset,
-            batch_size=self.cfg.train.bsz,
-            num_workers=self.cfg.num_workers,
-            shuffle=True,
-            drop_last=True,
-            pin_memory=self.use_cuda,
-        )
-        return loader
-
-    def val_dataloader(self):
-        dataset = MNIST(
-            os.getcwd(), train=False, download=True, transform=transforms.ToTensor()
-        )
-        loader = DataLoader(
-            dataset,
-            batch_size=self.cfg.train.val_bsz,
-            num_workers=self.cfg.num_workers,
-            shuffle=False,
-            drop_last=False,
-            pin_memory=self.use_cuda,
-        )
-        return loader
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.layers.parameters(), lr=1e-2,)
-
-    def training_step(self, batch, batch_nb):
-        x, y = batch
-        y_hat = self.layers(x.flatten(1))
-        loss = self.loss(y_hat, y)
-        return {
-            "loss": loss,
-            "progress_bar": {"batch_nb": batch_nb},
-            "log": {"batch_nb": batch_nb},
-        }
-
-    def validation_step(self, batch, batch_nb):
-        x, y = batch
-        y_hat = self.layers(x.flatten(1))
-        loss = self.loss(y_hat, y)
-        return {
-            "loss": loss,
-            "progress_bar": {"batch_nb": batch_nb},
-            "log": {"batch_nb": batch_nb},
-        }
-
-    def validation_epoch_end(self, outputs):
-        val_loss = torch.stack([o["loss"] for o in outputs]).mean()
-        log = {"val_loss": val_loss}
-        return {"val_loss": val_loss, "log": log, "progress_bar": log}
-
 
 if __name__ == "__main__":
-    args, cfg = parse_args_and_set_config(__C)
-    print(cfg)
+    args, cfg = parse_args_and_set_config(__C, blacklist=["gradient_clip_val"])
+    pp = PP(indent=4)
 
-    model = Model(cfg)
-    model.init_log()
-    loggers, ckpt_callback = model.make_lightning_loggers_ckpt()
-    trainer = pl.Trainer.from_argparse_args(
-        args,
-        logger=loggers,
-        checkpoint_callback=ckpt_callback,
-        max_epochs=cfg.train.epochs,
-        default_root_dir=model.exp_dir,
-    )
-    if args.eval:
-        pass
-    elif args.test:
-        pass
-    else:
-        pass
-        trainer.fit(model)
+    cfg.train.accumulate_grad_batches = args.accumulate_grad_batches
+    model = BasePLModel(cfg)
+
+    # Prints should be done after the init log
+    model.init_log(vars(args))
+    try:
+        _stdout = sys.stdout
+        _stderr = sys.stderr
+        with open(osp.join(model.exp_dir, "logfile.log"), "a") as log:
+            sys.stdout = Logger(log)
+            sys.stderr = ErrLogger(log)
+            pl._logger.addHandler(pl.python_logging.StreamHandler(sys.stdout))
+
+            if torch.cuda.is_available():
+                print("GPUS:", os.environ["CUDA_VISIBLE_DEVICES"])
+                print(torch.cuda.get_device_name())
+            print(pp.pformat(vars(args)))
+            print(pp.pformat(cfg))
+
+            loggers = model.make_lightning_loggers()
+
+            default_ckpt_callback_kwargs = {
+                "filepath": osp.join(model.exp_dir, "checkpoints/"),
+                "monitor": "val_loss",
+                "verbose": True,
+                "save_top_k": 2,
+            }
+            ckpt_callback = pl.callbacks.model_checkpoint.ModelCheckpoint(
+                **default_ckpt_callback_kwargs,
+            )
+            trainer = pl.Trainer.from_argparse_args(
+                args,
+                logger=loggers,
+                checkpoint_callback=ckpt_callback,
+                max_epochs=cfg.train.epochs,
+                default_root_dir=model.exp_dir,
+                gradient_clip_val=cfg.train.gradient_clip_val,
+            )
+            if args.eval:
+                pass
+            elif args.test:
+                pass
+            else:
+                pass
+                trainer.fit(model)
+    finally:
+        sys.stdout = _stdout
+        sys.stderr = _stderr
--- src/plb_base/base_pl_model.py
+++ src/plb_base/base_pl_model.py
@@ -11,15 +11,15 @@ from datetime import datetime as dt
 from pytorch_lightning.utilities.distributed import rank_zero_only
 import pytorch_lightning as pl
 
-from config import mkdir_p
+from plb_base.base_config import mkdir_p
 
 _plt = None
 
 
 class Logger(object):
-    def __init__(self, logfile):
+    def __init__(self, log):
         self.terminal = sys.stdout
-        self.log = open(logfile, "a")
+        self.log = log
 
     def write(self, message):
         self.terminal.write(message)
@@ -34,6 +34,26 @@ class Logger(object):
         pass
 
 
+# I'm not sure if this is needed, but is the first
+# patch that came to my mind
+class ErrLogger(object):
+    def __init__(self, log):
+        self.terminal = sys.stderr
+        self.log = log
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+        sys.stderr.flush()
+        self.log.flush()
+
+    def close(self):
+        pass
+
+    def flush(self):
+        pass
+
+
 class BasePLModel(pl.LightningModule):
     def __init__(self, hparams=None):
         super().__init__()
@@ -45,6 +65,10 @@ class BasePLModel(pl.LightningModule):
         #     self.load_from_checkpoint(hparams.resume)
         self.cfg = hparams
         self.comet_set = False
+        self._run_name = None
+        self.work_dir = osp.dirname(osp.dirname(osp.realpath(__file__)))
+        self.exp_dir = ""
+        self.exp_name = ""
 
     @property
     def use_cuda(self):
@@ -56,17 +80,25 @@ class BasePLModel(pl.LightningModule):
                 self.set_comet_graph()
                 self.log_src_comet()
                 self.log_cfg_comet()
-                self.print("\nSent src and config to comet")
+                # self.print("\nSent src and config to comet")
             self.comet_set = True  # Avoiding calling each time epoch starts
 
         return super().on_epoch_start()
+
+    @property
+    def loggers(self):
+        if self.logger:
+            if isinstance(self.logger, pl.loggers.base.LoggerCollection):
+                return self.logger
+            else:
+                return [self.logger]
 
     @property
     def comet_logger(self):
         comet_logger = getattr(self, "_comet_logger", None)
         if comet_logger:
             return comet_logger
-        for logger in self.logger:
+        for logger in self.loggers:
             if isinstance(logger, pl.loggers.CometLogger):
                 self._comet_logger = logger
                 return logger
@@ -82,17 +114,23 @@ class BasePLModel(pl.LightningModule):
             self.comet_logger.experiment.log_asset_folder(
                 osp.join(self.work_dir, "src"), recursive=True, log_file_name=True
             )
+            print("Sent src to comet")
 
     @rank_zero_only
     def log_cfg_comet(self):
-        if self.cfg.logcomet:
+        if self.cfg.logcomet and len(self.exp_dir):
             self.comet_logger.experiment.log_asset(osp.join(self.exp_dir, "cfg.json"))
             self.comet_logger.experiment.log_asset(osp.join(self.exp_dir, "cfg.yml"))
+            if os.path.exists(osp.join(self.exp_dir, "args.json")):
+                self.comet_logger.experiment.log_asset(
+                    osp.join(self.exp_dir, "args.json")
+                )
+            print("Sent config to comet")
 
     @rank_zero_only
     def log_figure(self, figure, name, step=None, close=True):
         if self.logger:
-            for logger in self.logger:
+            for logger in self.loggers:
                 if type(logger) is pl.loggers.TensorBoardLogger:
                     logger.experiment.add_figure(
                         name, figure, global_step=step, close=False
@@ -109,66 +147,82 @@ class BasePLModel(pl.LightningModule):
             plt = _plt
             plt.close(figure)
 
-    def make_lightning_loggers_ckpt(self, ckpt_callback_kwargs=dict(
-            # filepath=osp.join(self.exp_dir, "checkpoints/"),
-            monitor="val_loss",
-            verbose=True,
-            save_top_k=2,
+    @property
+    def run_name(self):
+        if self._run_name:
+            return self._run_name
+        return ""
 
-    )):
+    def make_comet_logger(self):
+        return pl.loggers.CometLogger(
+            api_key=os.environ["COMET_API_KEY"],
+            workspace=self.cfg.comet_workspace,
+            project_name=self.cfg.comet_project_name,
+            experiment_name=f"{self.exp_name}-{self.run_name}",
+        )
+
+    # def make_lightning_loggers_ckpt(
+    def make_lightning_loggers(
+        self,
+        # ckpt_callback_kwargs=dict(
+        #     # filepath=osp.join(self.exp_dir, "checkpoints/"),
+        #     monitor="val_loss",
+        #     verbose=True,
+        #     save_top_k=2,
+        # ),
+    ):
         loggers = [pl.loggers.TensorBoardLogger(save_dir=self.exp_dir, name="",)]
         if self.cfg.logcomet:
-            comet_logger = pl.loggers.CometLogger(
-                api_key=os.environ["COMET_API_KEY"],
-                workspace=self.cfg.comet_workspace,
-                project_name=self.cfg.comet_project_name,
-                experiment_name=self.run_name,
-            )
-            loggers.append(comet_logger)
+            loggers.append(self.make_comet_logger())
 
-        default_ckpt_callback_kwargs = {
-            'filepath': osp.join(self.exp_dir, "checkpoints/"),
-        }
-        default_ckpt_callback_kwargs.update(ckpt_callback_kwargs)
-        ckpt_callback = pl.callbacks.model_checkpoint.ModelCheckpoint(
-            **default_ckpt_callback_kwargs,
-        )
-        return loggers, ckpt_callback
-
+        # default_ckpt_callback_kwargs = {
+        #     "filepath": osp.join(self.exp_dir, "checkpoints/"),
+        # }
+        # default_ckpt_callback_kwargs.update(ckpt_callback_kwargs)
+        # ckpt_callback = pl.callbacks.model_checkpoint.ModelCheckpoint(
+        #     **default_ckpt_callback_kwargs,
+        # )
+        return loggers  # , ckpt_callback
 
     @rank_zero_only
-    def init_log(self):
+    def init_log(self, args=None):
         now = dt.now(tz.tzlocal())
         now = now.strftime("%m-%d-%Y-%H-%M-%S")
-        log_dir = self.cfg.exp_name
-        run_name = self.cfg.run_name
-        if run_name == "" or run_name is None:
-            run_name = now
-        self.run_name = run_name
-        work_dir = osp.dirname(osp.dirname(osp.realpath(__file__)))
-        self.work_dir = work_dir
-        exp_dir = osp.join(work_dir, "experiments", log_dir, run_name)
-        _exp_dir = exp_dir
-        i = 2
-        while os.path.exists(_exp_dir):
-            _exp_dir = f"{exp_dir}-{i}"
-            i += 1
+        self.exp_name = self.cfg.exp_name
+        log_dir = self.exp_name
+        _run_name = self.cfg.run_name
+        run_name = _run_name
+        if _run_name == "" or _run_name is None:
+            _run_name = now
+        _exp_dir = osp.join(self.work_dir, "experiments", log_dir, _run_name)
         exp_dir = _exp_dir
+        i = 2
+        while os.path.exists(exp_dir):
+            exp_dir = f"{_exp_dir}-{i}"
+            run_name = f"{_run_name}-{i}"
+            i += 1
+        self._run_name = run_name
+        # exp_dir = exp_dir
 
         self.exp_dir = exp_dir
         shutil.copytree(
-            osp.join(work_dir, "src"),
-            osp.join(exp_dir, "src"),
+            osp.join(self.work_dir, "src"),
+            osp.join(self.exp_dir, "src"),
             ignore=shutil.ignore_patterns(".*", "__pycache__", ".DS_Store"),
         )
 
-        self.logfile = osp.join(exp_dir, "logfile.log")
-        sys.stdout = Logger(self.logfile)
+        # self.logfile = osp.join(exp_dir, "logfile.log")
+        # log = open(self.logfile, "a")
+        # sys.stdout = Logger(log)
+        # sys.stderr = ErrLogger(log)
 
         with open(osp.join(self.exp_dir, "cfg.json"), "w") as f:
             json.dump(self.cfg, f, indent=4)
         with open(osp.join(self.exp_dir, "cfg.yml"), "w") as f:
             yaml.dump(json.loads(json.dumps(self.cfg)), f, indent=4)
+        if args is not None:
+            with open(osp.join(self.exp_dir, "args.json"), "w") as f:
+                json.dump(args, f, indent=4)
 
         # print(exp_dir)
 
